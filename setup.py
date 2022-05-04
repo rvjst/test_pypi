df1e27bc93a62443c3fd47bc7183d5a511ab7f97
rvjst <90255680+rvjst@users.noreply.github.com>
2022-05-04 11:35:55 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 import setuptools
 
-setuptools.setup {
+setuptools.setup (
   include_package_data = True,
   name='mycalc_pypi',
-  version='0,0,1',
+  version='0.0.1',
   description = 'oss-dev my calculator_pypi',
   author='rvjst',
   author_email='heyans93@gmail.com',
   url = "https://github.com/rvjst/test_pypi/new/main",
-  download_url='https://github.com/rvjst/test_pypi/releases/tag/v0.0.1.zip",
+  download_url='https://github.com/rvjst/test_pypi/archive/refs/tags/v0.0.1.zip",
   install_requires=["pytest"],
   long_description = 'oss-dev calculator python module',
   long_description_content_type = 'text/markdown',
@@ -16,5 +16,5 @@ setuptools.setup {
      "Programming Language :: Python :: 3",
      "Operating System :: OS Independent",
   ],
-}
+)
   
